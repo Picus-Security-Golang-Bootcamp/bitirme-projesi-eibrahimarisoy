36dc86a86c662cc4b5ab3d19b4e25f4c886af95c
eibrahimarisoy <eibrahimarisoy@gmail.com>
2022-04-15 18:39:56 +0300

    added category service test

--- internal/category/service_test.go
+++ internal/category/service_test.go
@@ -3,14 +3,17 @@ package category
 import (
 	"errors"
 	"patika-ecommerce/internal/model"
+	"reflect"
 	"testing"
 
+	"github.com/go-playground/assert/v2"
 	"github.com/google/uuid"
 )
 
 func TestCategoryService_CreateCategory(t *testing.T) {
 	categoryName := "category name 1"
 	categoryDescription := "category description 1"
+
 	type fields struct {
 		categoryRepo MockCategoryRepository
 	}
@@ -75,6 +78,171 @@ func TestCategoryService_CreateCategory(t *testing.T) {
 	}
 }
 
+func TestCategoryService_GetCategories(t *testing.T) {
+	categoryName := "category name 1"
+	categoryDescription := "category description 1"
+
+	type fields struct {
+		categoryRepo MockCategoryRepository
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		want    *[]model.Category
+		wantErr bool
+	}{
+		{
+			name: "categoryService_GetCategories_Success",
+			fields: fields{
+				categoryRepo: &mockCategoryRepository{
+					items: []model.Category{
+						{
+							Name:        &categoryName,
+							Description: categoryDescription,
+						},
+					},
+				},
+			},
+			want: &[]model.Category{
+				{
+					Name:        &categoryName,
+					Description: categoryDescription,
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &CategoryService{
+				categoryRepo: tt.fields.categoryRepo,
+			}
+			got, err := c.GetCategories()
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CategoryService.GetCategories() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("CategoryService.GetCategories() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCategoryService_GetCategoryByID(t *testing.T) {
+	categoryName := "category name 1"
+	categoryDescription := "category description 1"
+	id := uuid.New()
+
+	type fields struct {
+		categoryRepo MockCategoryRepository
+	}
+	type args struct {
+		id uuid.UUID
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *model.Category
+		wantErr bool
+	}{
+		{
+			name: "categoryService_GetCategoryByID_Success",
+			fields: fields{
+				categoryRepo: &mockCategoryRepository{
+					items: []model.Category{
+						{
+							Base:        model.Base{ID: id},
+							Name:        &categoryName,
+							Description: categoryDescription,
+						},
+					},
+				},
+			},
+			args: args{
+				id: id,
+			},
+			want: &model.Category{
+				Base:        model.Base{ID: id},
+				Name:        &categoryName,
+				Description: categoryDescription,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &CategoryService{
+				categoryRepo: tt.fields.categoryRepo,
+			}
+			got, err := c.GetCategoryByID(tt.args.id)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CategoryService.GetCategoryByID() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("CategoryService.GetCategoryByID() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCategoryService_UpdateCategory(t *testing.T) {
+	categoryName := "category name 1"
+	categoryDescription := "category description 1"
+	id := uuid.New()
+
+	newCategoryName := "new category name"
+	newCategoryDescription := "new category description"
+
+	type fields struct {
+		categoryRepo MockCategoryRepository
+	}
+	type args struct {
+		category *model.Category
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "categoryService_GetCategoryByID_Success",
+			fields: fields{
+				categoryRepo: &mockCategoryRepository{
+					items: []model.Category{
+						{
+							Base:        model.Base{ID: id},
+							Name:        &categoryName,
+							Description: categoryDescription,
+						},
+					},
+				},
+			},
+			args: args{
+				category: &model.Category{
+					Base:        model.Base{ID: id},
+					Name:        &newCategoryName,
+					Description: newCategoryDescription,
+				},
+			},
+			wantErr: false,
+		}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &CategoryService{
+				categoryRepo: tt.fields.categoryRepo,
+			}
+			if err := c.UpdateCategory(tt.args.category); (err != nil) != tt.wantErr {
+				t.Errorf("CategoryService.UpdateCategory() error = %v, wantErr %v", err, tt.wantErr)
+				assert.Equal(t, tt.args.category.Name, newCategoryName)
+			}
+		})
+	}
+}
+
 type mockCategoryRepository struct {
 	items []model.Category
 }
@@ -91,30 +259,28 @@ func (r *mockCategoryRepository) InsertCategory(category *model.Category) error 
 
 // GetCategories returns all categories
 func (r *mockCategoryRepository) GetCategories() (*[]model.Category, error) {
-	categories := &[]model.Category{}
-	// //  &[]models.Book{}
-	// if err := r.db.Find(categories).Error; err != nil {
-	// 	return nil, err
-	// }
-
-	return categories, nil
+	return &r.items, nil
 }
 
 // GetCategoryByID returns a category by id
 func (r *mockCategoryRepository) GetCategoryByID(id uuid.UUID) (*model.Category, error) {
 	category := &model.Category{}
-	// if err := r.db.Where("id = ?", id).First(category).Error; err != nil {
-	// 	return nil, err
-	// }
+	for _, item := range r.items {
+		if item.ID == id {
+			category = &item
+		}
+	}
 	return category, nil
 }
 
 // UpdateCategory updates a category with the given id
 func (r *mockCategoryRepository) UpdateCategory(category *model.Category) error {
-	// result := r.db.Updates(category)
-	// if result.Error != nil {
-	// 	return result.Error
-	// }
+	for i, item := range r.items {
+		if item.ID == category.ID {
+			r.items[i] = *category
+		}
+	}
+
 	return nil
 }
 
@@ -135,9 +301,76 @@ func (r *mockCategoryRepository) InsertBulkCategory(categories *[]model.Category
 
 // DeleteCategory deletes a category by id
 func (r *mockCategoryRepository) DeleteCategory(category *model.Category) error {
-	// result := r.db.Select(clause.Associations).Delete(category)
-	// if result.Error != nil {
-	// 	return result.Error
-	// }
+	isExist := false
+	for index, item := range r.items {
+		if item.ID == category.ID {
+			category = &item
+			isExist = true
+			r.items = append(r.items[:index], r.items[index+1:]...)
+		}
+	}
+	if !isExist {
+		return errors.New("category not found")
+	}
 	return nil
+}
+
+func TestCategoryService_DeleteCategory(t *testing.T) {
+	id := uuid.New()
+	idTwo := uuid.New()
+	type fields struct {
+		categoryRepo MockCategoryRepository
+	}
+	type args struct {
+		id uuid.UUID
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "categoryService_DeleteCategory_Success",
+			fields: fields{
+				categoryRepo: &mockCategoryRepository{
+					items: []model.Category{
+						{
+							Base: model.Base{ID: id},
+						},
+					},
+				},
+			},
+			args: args{
+				id: id,
+			},
+			wantErr: false,
+		},
+		{
+			name: "categoryService_DeleteCategory_Success",
+			fields: fields{
+				categoryRepo: &mockCategoryRepository{
+					items: []model.Category{
+						{
+							Base: model.Base{ID: id},
+						},
+					},
+				},
+			},
+			args: args{
+				id: idTwo,
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &CategoryService{
+				categoryRepo: tt.fields.categoryRepo,
+			}
+			if err := c.DeleteCategory(tt.args.id); (err != nil) != tt.wantErr {
+				t.Errorf("CategoryService.DeleteCategory() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
 }
