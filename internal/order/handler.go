ff457f45a8142c51bb4e6f134cba9d34e318433a
eibrahimarisoy <eibrahimarisoy@gmail.com>
2022-04-16 16:58:01 +0300

    added completeOrder handler test

--- internal/order/handler.go
+++ internal/order/handler.go
@@ -6,6 +6,7 @@ import (
 	"patika-ecommerce/internal/model"
 	"patika-ecommerce/pkg/config"
 	paginationHelper "patika-ecommerce/pkg/pagination"
+	common "patika-ecommerce/pkg/utils"
 
 	mw "patika-ecommerce/pkg/middleware"
 
@@ -15,11 +16,11 @@ import (
 )
 
 type orderHandler struct {
-	orderService *OrderService
+	orderRepo MockOrderRepository
 }
 
-func NewOrderHandler(r *gin.RouterGroup, cfg *config.Config, orderService *OrderService) {
-	handler := &orderHandler{orderService: orderService}
+func NewOrderHandler(r *gin.RouterGroup, cfg *config.Config, orderRepo *OrderRepository) {
+	handler := &orderHandler{orderRepo: orderRepo}
 
 	r.Use(mw.AuthenticationMiddleware(cfg.JWTConfig.SecretKey))
 	r.POST("", handler.completeOrder)
@@ -43,7 +44,15 @@ func (r *orderHandler) completeOrder(c *gin.Context) {
 
 	user := c.MustGet("user").(*model.User)
 
-	order, err := r.orderService.CompleteOrder(user, reqBody)
+	cartId, err := common.StrfmtToUUID(*reqBody.CartID)
+	if err != nil {
+		c.JSON(httpErr.ErrorResponse(err))
+	}
+
+	order, err := r.orderRepo.CompleteOrder(user, cartId)
+	if err != nil {
+		c.JSON(httpErr.ErrorResponse(err))
+	}
 
 	if err != nil {
 		c.JSON(httpErr.ErrorResponse(err))
@@ -58,7 +67,7 @@ func (r *orderHandler) listOrders(c *gin.Context) {
 	user := c.MustGet("user").(*model.User)
 	pagination := c.MustGet("pagination").(*paginationHelper.Pagination)
 
-	data, err := r.orderService.GetOrdersByUser(user, pagination)
+	data, err := r.orderRepo.GetOrdersByUser(user, pagination)
 
 	if err != nil {
 		c.JSON(httpErr.ErrorResponse(err))
@@ -78,7 +87,7 @@ func (r *orderHandler) cancelOrder(c *gin.Context) {
 		return
 	}
 
-	if err := r.orderService.CancelOrder(user, id); err != nil {
+	if err := r.orderRepo.CancelOrder(id, user); err != nil {
 		c.JSON(httpErr.ErrorResponse(err))
 		return
 	}
--- pkg/router/router.go
+++ pkg/router/router.go
@@ -54,7 +54,6 @@ func InitializeRoutes(rootRouter *gin.RouterGroup, db *gorm.DB, cfg *config.Conf
 	orderRepo.Migration()
 	orderItemRepo := order.NewOrderItemRepository(db)
 	orderItemRepo.Migration()
-	orderService := order.NewOrderService(orderRepo, orderItemRepo)
-	order.NewOrderHandler(orderGroup, cfg, orderService)
+	order.NewOrderHandler(orderGroup, cfg, orderRepo)
 
 }
