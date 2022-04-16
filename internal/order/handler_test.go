9528a9b79d6c7634f174f874f4444170d9a8ddbf
eibrahimarisoy <eibrahimarisoy@gmail.com>
2022-04-16 17:34:31 +0300

    added list and cancel order handler test

--- internal/order/handler_test.go
+++ internal/order/handler_test.go
@@ -2,6 +2,7 @@ package order
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -65,11 +66,10 @@ func Test_orderHandler_completeOrder(t *testing.T) {
 	orderHandler := &orderHandler{
 		orderRepo: orderRepo,
 	}
-	c, r := gin.CreateTestContext(w)
-	r.POST("/cart/items/:id", orderHandler.completeOrder)
+	c, _ := gin.CreateTestContext(w)
+	c.Set("user", &user)
 	c.Request, _ = http.NewRequest("POST", "/orders", nil)
 	c.Request.Header.Set("Content-Type", "application/json")
-	c.Set("user", &user)
 	c.Request.Body = ioutil.NopCloser(bytes.NewBuffer(getOrderCompletePayload(cartId.String())))
 
 	orderHandler.completeOrder(c)
@@ -79,6 +79,123 @@ func Test_orderHandler_completeOrder(t *testing.T) {
 	assert.Equal(t, 1, len(orderRepo.orders))
 
 }
+
+func Test_orderHandler_listOrders(t *testing.T) {
+	cartId := uuid.New()
+	productId := uuid.New()
+	userId := uuid.New()
+
+	productName := "product name"
+	productStock := int64(10)
+
+	orderRepo := &mockOrderRepo{
+		orders: []model.Order{
+			{
+				CartID: cartId,
+				Status: model.OrderStatusCompleted,
+				Base:   model.Base{ID: uuid.New()},
+				Items: []model.OrderItem{
+					{
+						ProductID: productId,
+						Price:     100,
+						Product: model.Product{
+							Base:  model.Base{ID: productId},
+							Name:  &productName,
+							Price: 100,
+							Stock: &productStock,
+						},
+					},
+				},
+				UserID:     userId,
+				TotalPrice: 100,
+			},
+		},
+	}
+	pagination := paginationHelper.Pagination{
+		Limit: 2,
+		Page:  1,
+	}
+
+	gin.SetMode(gin.TestMode)
+
+	user := model.User{
+		Base: model.Base{ID: userId},
+	}
+
+	w := httptest.NewRecorder()
+	orderHandler := &orderHandler{
+		orderRepo: orderRepo,
+	}
+	c, _ := gin.CreateTestContext(w)
+	c.Set("pagination", &pagination)
+	c.Set("user", &user)
+	c.Request, _ = http.NewRequest("GET", "/orders", nil)
+	c.Request.Header.Set("Content-Type", "application/json")
+
+	orderHandler.listOrders(c)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+}
+
+func Test_orderHandler_cancelOrder(t *testing.T) {
+	cartId := uuid.New()
+	productId := uuid.New()
+	userId := uuid.New()
+	orderId := uuid.New()
+
+	productName := "product name"
+	productStock := int64(10)
+
+	orderRepo := &mockOrderRepo{
+		orders: []model.Order{
+			{
+				Base:   model.Base{ID: orderId},
+				CartID: cartId,
+				Status: model.OrderStatusCompleted,
+				Items: []model.OrderItem{
+					{
+						ProductID: productId,
+						Price:     100,
+						Product: model.Product{
+							Base:  model.Base{ID: productId},
+							Name:  &productName,
+							Price: 100,
+							Stock: &productStock,
+						},
+					},
+				},
+				UserID:     userId,
+				TotalPrice: 100,
+			},
+		},
+	}
+
+	gin.SetMode(gin.TestMode)
+
+	user := model.User{
+		Base: model.Base{ID: userId},
+	}
+
+	w := httptest.NewRecorder()
+	orderHandler := &orderHandler{
+		orderRepo: orderRepo,
+	}
+	c, _ := gin.CreateTestContext(w)
+	c.Set("user", &user)
+	c.Params = []gin.Param{{Key: "id", Value: orderId.String()}}
+	c.Request, _ = http.NewRequest("DELETE", "/orders/:id", nil)
+	c.Request.Header.Set("Content-Type", "application/json")
+
+	orderHandler.cancelOrder(c)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, int64(11), *orderRepo.orders[0].Items[0].Product.Stock)
+}
+
+var (
+	OrderCannotBeCanceledError = fmt.Errorf("order cannot be canceled")
+	OrderNotFoundError         = fmt.Errorf("order not found")
+)
 
 type mockOrderRepo struct {
 	orders     []model.Order
@@ -125,7 +242,13 @@ func (r *mockOrderRepo) CompleteOrder(user *model.User, cartId uuid.UUID) (*mode
 
 // GetOrdersByUser returns all orders of a user
 func (r *mockOrderRepo) GetOrdersByUser(user *model.User, pagination *paginationHelper.Pagination) (*paginationHelper.Pagination, error) {
-	return nil, nil
+	orders := []*model.Order{}
+	for _, order := range r.orders {
+		orders = append(orders, &order)
+	}
+	pagination.TotalRows = int64(len(orders))
+	pagination.Rows = OrdersToOrderDetailedResponse(orders)
+	return pagination, nil
 }
 
 // GetOrderByIdAndUser returns an order by id and user
@@ -135,55 +258,19 @@ func (r *mockOrderRepo) GetOrderByIdAndUser(user *model.User, id uuid.UUID) (*mo
 
 // CancelOrder cancels an order
 func (r *mockOrderRepo) CancelOrder(id uuid.UUID, user *model.User) error {
-	return nil
+	for _, order := range r.orders {
+		if order.ID == id && order.UserID == user.ID && order.Status == model.OrderStatusCompleted {
+			if order.IsCancelable() {
+				order.Status = model.OrderStatusCanceled
+
+				for _, item := range order.Items {
+					*item.Product.Stock += 1
+				}
+
+				return nil
+			}
+			return OrderCannotBeCanceledError
+		}
+	}
+	return OrderNotFoundError
 }
-
-// func TestOrderService_CompleteOrder(t *testing.T) {
-
-// 	type fields struct {
-// 		orderRepo *mockOrderRepo
-// 	}
-// 	type args struct {
-// 		user *model.User
-// 		req  *api.OrderRequest
-// 	}
-// 	tests := []struct {
-// 		name    string
-// 		fields  fields
-// 		args    args
-// 		want    *model.Order
-// 		wantErr bool
-// 	}{
-// 		{
-// 			name: "success",
-// 			fields: fields{
-// 				orderRepo: orderRepo,
-// 			},
-// 			args: args{
-// 				user: &model.User{
-// 					Base: model.Base{ID: userId},
-// 				},
-// 				req: &api.OrderRequest{
-// 					CartID: &reqCartId,
-// 				},
-// 			},
-// 			want: &model.Order{},
-// 		},
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			r := &OrderService{
-// 				orderRepo: tt.fields.orderRepo,
-// 			}
-// 			_, err := r.CompleteOrder(tt.args.user, tt.args.req)
-// 			if (err != nil) != tt.wantErr {
-// 				t.Errorf("OrderService.CompleteOrder() error = %v, wantErr %v", err, tt.wantErr)
-// 				return
-// 			}
-
-// 			assert.Equal(t, int64(9), *orderRepo.products[0].Stock)
-// 			assert.Equal(t, 1, len(orderRepo.orders))
-
-// 		})
-// 	}
-// }
